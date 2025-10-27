033f687a83af29fac2c74fdef556db5c8aaf678e
Tolnai Levente <levi.tolnai7@gmail.com>
2025-10-27 21:12:13 +0100

    AuthController Register done

--- budgetmaster/BudgetMaster.Endpoint/Controllers/AuthenticationController.cs
+++ budgetmaster/BudgetMaster.Endpoint/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using BudgetMaster.Endpoint.Controllers.Common;
 using BudgetMaster.Entities.DTOs.User;
+using BudgetMaster.Logic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,18 +9,33 @@ namespace BudgetMaster.Endpoint.Controllers
     [Route("auth")]
     public class AuthenticationController : ApiControllerBase
     {
+        private readonly AuthenticationLogic _logic;
 
-        //_logic;
-        public AuthenticationController()
+        public AuthenticationController(AuthenticationLogic logic)
         {
-                
+            _logic = logic;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto dto)
         {
-            //_logic.method();
-            return Ok();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _logic.RegisterAsync(dto);
+
+            if (!result.Success)
+            {
+                return BadRequest(new { message = result.Message });
+            }
+
+            return Ok(new
+            {
+                message = result.Message,
+                token = result.Token
+            });
         }
 
         [HttpPost("login")]
