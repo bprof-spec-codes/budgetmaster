1407227eea0aad50a4e08701caed4486c617045c
krisz1321 <erdeikrisz1321@gmail.com>
2025-11-01 17:45:31 +0100

    add authentication and navigation

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginModel } from '../models/loginmodel';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+	loginModel: LoginModel = new LoginModel();
+	 errorMessage: string = '';
+
+	 constructor(public authService: AuthService){}
+
+	inputCheck(): boolean {
+    	return !(
+			this.loginModel.email.length > 5 && 
+			this.loginModel.email.includes('@')&& 
+			this.loginModel.email.includes('.')&& 
+			this.loginModel.password.length > 3
+		)
+  	}
+
+	onLogin(): void {
+    this.errorMessage = '';
+    
+    this.authService.login(this.loginModel).subscribe({
+      next: (response) => {
+        // Sikeres bejelentkezés
+        console.log('Login successful', response);
+       // this.router.navigate(['/']);
+      },
+      error: (error) => {
+        // Hiba kezelése
+        console.error('Login failed:', error);
+        this.errorMessage = 'Hibás email vagy jelszó!';
+      }
+    });
+  }
+	
+
+
+
+
 }
+
