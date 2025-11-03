64a24524e0a87054fb9737888afc89bafed6937e
Tolnai Levente <levi.tolnai7@gmail.com>
2025-11-03 11:36:11 +0100

    OrganizationId nullable migration

---
+++ budgetmaster/BudgetMaster.Data/Migrations/20251103103343_OrganizationIdNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetMaster.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrganizationIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizationId",
+                table: "Transactions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizationId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
